3332727fff249665d90a4f42e6a98fc7c7a06109
Patrick Quinn-Graham <patrick@cole.local>
2010-05-02 18:58:13 +1000

    iPhone friendlyness

--- thepatrick.com.au/_static/thepatrick.js
+++ thepatrick.com.au/_static/thepatrick.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 	$('a[rel]').click(function(){
-		$("." + $(this).attr("rel")).show("slow");
-		$(this).addClass("active");
+	  if($(this).is(".active")) {
+  		$("." + $(this).attr("rel")).hide("slow");
+  		$(this).removeClass("active");
+	  } else {
+  		$("." + $(this).attr("rel")).show("slow");
+  		$(this).addClass("active");
+	  }
 		return false;
 	});
 });
