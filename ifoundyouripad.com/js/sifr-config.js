a03eae8cc912169edb48dc161774e6007e05b117
Patrick Quinn-Graham <make-contact@pftqg.com>
2010-08-16 22:58:05 +1000

    I Found Your iPad (a joke site, for now)

---
+++ ifoundyouripad.com/js/sifr-config.js
@@ -0,0 +1,14 @@
+ var se02006d = {
+      src: 'flash/se02006d.swf'
+ };
+
+ sIFR.activate(se02006d);
+sIFR.replace(se02006d, {
+
+       selector: 'h1.head',
+css: [
+      '.sIFR-root { width:auto;padding:0 0 10px 0; font-size:36px; font-weight:normal; color:#4d300f; }'
+      ],
+       wmode: 'transparent'
+ });
+
